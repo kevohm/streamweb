ac087df9f0c3a434f7c7923844f7c317754c7e25
kevohm <kevinkbet@gmail.com>
2025-04-03 17:35:42 +0300

    feat: added server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  url = environment.apiUrl;
   title = 'angular-tutorial';
 }
